da8ba44c867f092d146a1628c6f4779f562c259c
Lauryn Thayne <lalder57@gmail.com>
2024-08-13 14:35:48 -0600

    all parts working, just looks ugly haha

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
+import sample from 'lodash.sample'
 
 const app = express();
 const port = '8000';
@@ -45,8 +46,41 @@ app.get('/hello', (req, res) => {
   res.render('hello.html');
 });
 
+
+let name;
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
-  const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  
+  name = req.query.name || 'stranger';
+  const compliment = sample(COMPLIMENTS);
+  res.render('greet.html.njk', { 
+    name: name, 
+    compliment: compliment,
+ });
+});
+
+// See if the person wants to play. If so, take them to the game page. 
+// If not, take them to the goodbye page.
+app.get("/game", (req, res) => {
+  const play = req.query.play;
+  
+  if (play === "no") {
+    res.render("goodbye.html.njk", {
+      name: name,
+    });
+  } else {
+    res.render("game.html.njk");
+  }
+});
+
+// Display the results the user input from the game page on the madlib page.
+app.get("/madlib", (req, res) => {
+  const {person, color, noun, adjective} = req.query;
+
+  res.render("madlib.html.njk", {
+    person: person,
+    color: color, 
+    noun: noun,
+    adjective: adjective,
+  });
 });
